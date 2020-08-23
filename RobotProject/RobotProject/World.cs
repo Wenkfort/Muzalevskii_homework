3421ab29f875d9b44f69ae4e1db3df38b16f8372
Ivan Kornev <wenkfort@gmail.com>
2020-08-23 22:02:29 +0300

    NEW: init repo

---
+++ RobotProject/RobotProject/World.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotProject
+{
+    public class World  //класс со всеми объектами виртуальной среды
+    {
+        public Robot r; //робот
+        public List<Obstacle> obstacles = new List<Obstacle>(); //список препятствий
+
+        public World()
+        {
+            r = new Robot() { x = 50, y = 50, a = 0, w = 50, h = 100 };
+
+            obstacles.Add(new Obstacle { x = 50, y = 80, d = 50 });
+            obstacles.Add(new Obstacle { x = 150, y = 110, d = 60 });
+            obstacles.Add(new Obstacle { x = 90, y = 150, d = 40 });
+        }
+
+        public void Draw(Graphics g)
+        {
+            r.Draw(g);
+            foreach (var o in obstacles) o.Draw(g);
+        }
+
+        public void Sim(float dt)
+        {
+            r.Sim(dt, this);
+        }
+    }
+}
